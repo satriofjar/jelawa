d4236d70c1917e3109e5ff6da19404cd4e98554d
satrio <satrio0226@gmail.com>
2025-07-30 12:29:25 +0700

    add .env and change db to mysql

--- base/admin.py
+++ base/admin.py
@@ -13,3 +13,8 @@ admin.site.register(Island)
 admin.site.register(City)
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Answer)
+
+# Admin site custiom title
+admin.site.site_header = "Jelawa Administrator"
+admin.site.site_title = "Jelawa Admin"
+admin.site.index_title = "Welcome to Jelawa Administrator"
